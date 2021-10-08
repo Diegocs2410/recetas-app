cf4d9fe85ce288e5781f5d2a7e087d68116719c2
Diego Armando Carreño Serrano <Diegocs2410@gmail.com>
2021-10-08 16:15:21 -0500

    Inicializando proyecto

---
+++ src/components/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const Login = () => {
+  return (
+    <div className='container'>
+      <div className='row'>
+        <div className='col-md-4 col-lg-6 '>
+
+          <h2 className='display-3'>Login</h2>
+        </div>
+        <div className='col-md-8 col-lg-6'>
+          <h2 className='display-3'>Imagenes receta</h2>
+        </div>
+      </div>
+    </div>
+  );  
+};
---
+++ src/components/Root.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Root = () => {
+  return <h2 className='display-4 text-center text-primary text-center '>Pagina Principal</h2>;
+};
