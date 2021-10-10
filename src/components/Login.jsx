2cf33bc9049200eb5d09624fa0182aad50827dc5
Diego Armando Carreño Serrano <Diegocs2410@gmail.com>
2021-10-09 19:45:49 -0500

    Cambio colores login y letrero login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -31,11 +31,10 @@ export const Login = () => {
   return (
     <div style={{ overflow: 'hidden' }}>
       <svg
-        id='svg-login'
         preserveAspectRatio='none'
         viewBox='0 0 1200 120'
         xmlns='http://www.w3.org/2000/svg'
-        style={{ fill: '#F5C6A5', width: '113%', height: 182, transform: 'scaleX(-1)' }}
+        style={{ fill: '#3e3d3d', width: '113%', height: 182, transform: 'scaleX(-1)' }}
       >
         <path d='M321.39 56.44c58-10.79 114.16-30.13 172-41.86 82.39-16.72 168.19-17.73 250.45-.39C823.78 31 906.67 72 985.66 92.83c70.05 18.48 146.53 26.09 214.34 3V0H0v27.35a600.21 600.21 0 00321.39 29.09z' />
       </svg>
@@ -43,11 +42,11 @@ export const Login = () => {
         <div className='row '>
           <div className='col-md-8 col-lg-6 imgLogin order-2 order-md-0 order-lg-0 m-2 mx-auto'></div>
           <div className='col-md-4 m-2 my-md-auto mx-md-auto order-0 order-md-1'>
-            <h2 className='display-3 text-center title-login'>Login</h2>
+            <h2 className='text-center title-login'>Encuentra las mejores recetas!!</h2>
             <form onSubmit={hanldeSubmit}>
               <input
                 type='email'
-                className='form-control mt-2'
+                className='form-control mt-2 rounded-pill'
                 placeholder='email@email.com'
                 name='email'
                 value={userData.email}
@@ -55,18 +54,33 @@ export const Login = () => {
               />
               <input
                 type='password'
-                className='form-control mt-2'
+                className='form-control mt-2 rounded-pill'
                 placeholder='Password'
                 name='password'
                 value={userData.password}
                 onChange={handleOnChange}
               />
-              <button type='submit' className='btn btn-primary form-control my-2'>
+              <button type='submit' className='btn btn-primary form-control my-2 btn-sm p-2'>
                 Iniciar Sesión
               </button>
             </form>
           </div>
         </div>
+      </div>
+      <div style={{ overflow: 'hidden' }}>
+        <svg
+          preserveAspectRatio='none'
+          viewBox='0 0 1200 120'
+          xmlns='http://www.w3.org/2000/svg'
+          style={{
+            fill: '#3e3d3d',
+            width: '113%',
+            height: 182,
+            transform: 'rotate(180deg) scaleX(-1)',
+          }}
+        >
+          <path d='M321.39 56.44c58-10.79 114.16-30.13 172-41.86 82.39-16.72 168.19-17.73 250.45-.39C823.78 31 906.67 72 985.66 92.83c70.05 18.48 146.53 26.09 214.34 3V0H0v27.35a600.21 600.21 0 00321.39 29.09z' />
+        </svg>
       </div>
     </div>
   );
